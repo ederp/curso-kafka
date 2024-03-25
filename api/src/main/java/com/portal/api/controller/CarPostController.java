d95c51ffab5572ed7d9532d9f640765450887362
ederp <epmutley@gmail.com>
2024-03-25 14:50:36 -0300

    Criação dos producers Kafka

--- api/src/main/java/com/portal/api/controller/CarPostController.java
+++ api/src/main/java/com/portal/api/controller/CarPostController.java
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.portal.api.dto.CarPostDTO;
+import com.portal.api.message.KafkaProducerMessage;
 import com.portal.api.service.CarPostStoreService;
 
 @RestController
@@ -23,19 +25,28 @@ public class CarPostController {
 	@Autowired
 	private CarPostStoreService carPostStoreService;
 	
+	@Autowired
+	private KafkaProducerMessage kafkaProducerMessage;
+	
+	@PostMapping("/post")
+	public ResponseEntity<Object> postCarForSale(@RequestBody CarPostDTO carPostDTO) {
+		kafkaProducerMessage.sendMessage(carPostDTO);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+	
 	@GetMapping("/posts")
 	public ResponseEntity<List<CarPostDTO>> getCarSales(){
 		return ResponseEntity.status(HttpStatus.FOUND).body(carPostStoreService.getCarForSales());
 	}
 	
 	@PutMapping("/{id}")
-	public ResponseEntity changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id) {
+	public ResponseEntity<Object> changeCarSale(@RequestBody CarPostDTO carPostDTO, @PathVariable("id") String id) {
 		carPostStoreService.changeCarForSale(carPostDTO, id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 	
 	@DeleteMapping("/{id}")
-	public ResponseEntity deleteCarForSale(@PathVariable("id") String id) {
+	public ResponseEntity<Object> deleteCarForSale(@PathVariable("id") String id) {
 		carPostStoreService.removeCarForSale(id);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
