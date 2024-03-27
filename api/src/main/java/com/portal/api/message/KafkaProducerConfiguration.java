149b38c28efcccce6d34cda27183ee73358eeba0
ederp <epmutley@gmail.com>
2024-03-27 19:08:16 -0300

    Correção de algumas classes para executar o producer com Kafka

--- api/src/main/java/com/portal/api/message/KafkaProducerConfiguration.java
+++ api/src/main/java/com/portal/api/message/KafkaProducerConfiguration.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +14,13 @@ import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.databind.ser.std.StringSerializer;
 import com.portal.api.dto.CarPostDTO;
 
 @Component
 @Configuration
 public class KafkaProducerConfiguration {
 
-	@Value("${spring.kafka.bootstrap.servers}")
+	@Value("${spring.kafka.bootstrap-servers}")
 	private String bootstrapServer;
 	
 	@Bean
